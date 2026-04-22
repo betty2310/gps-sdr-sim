ba66a5e5351ff50cd1cfcf02099a41d3b1b35f34
betty2310 <huynh.dh2310@gmail.com>
2026-04-22 13:36:13 +0700

    update script download ephemeris file from cddis

--- download_nasa_ephemeris.sh
+++ download_nasa_ephemeris.sh
@@ -6,13 +6,13 @@ year="$(date +%Y)"
 yy="$(date +%y)"
 doy="$(date +%j)"
 
-base_name="brdc${doy}0.${yy}n"
+base_name="hour${doy}0.${yy}n"
 gz_file="${base_name}.gz"
-url="https://cddis.nasa.gov/archive/gnss/data/daily/${year}/${doy}/${yy}n/${gz_file}"
+url="https://cddis.nasa.gov/archive/gnss/data/hourly/${year}/${doy}/${gz_file}"
 
 echo "Downloading URL: ${url}"
 
-wget "$url"
+wget --auth-no-challenge "$url"
 
 gzip -df "$gz_file"
 
